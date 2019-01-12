d7a8cb350a598cc237d19db869b55b454d163817
swanjala <wanjala.sam.s@gmail.com>
2019-01-12 21:19:00 +0300

    (refactor): implement review changes
    - Remove unused project depencency in app/gradle
    - Add unit test for app engine's async task
    - Remove unecessary tests
    - Refactor async task to seperate class for testablity
    - Optimize endpoint class

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,27 +1,17 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.nanodegree.sam.mainfragment.MainActivityDisplay;
-import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
-
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static String NAME = "Manfred";
 
 
     @Override
@@ -35,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 new EndpointAsyncTask()
-                        .execute(new Pair<Context, String>(getBaseContext(), "Manfred"));
+                        .execute(new Pair<Context, String>(getBaseContext(), NAME));
             }
         });
 
@@ -59,49 +49,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class EndpointAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-        private MyApi myApiService = null;
-        private Context context;
-        @Override
-        protected String doInBackground(Pair<Context, String>... params) {
-            if (myApiService == null) {
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api")
-                        .setApplicationName("Humlarious")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
-                                    throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                myApiService = builder.build();
-
-            }
-            context = params[0].first;
-            String name = params[0].second;
-
-            try {
-                return myApiService.sayHi(name).execute().getData().concat(
-                        " \n" + myApiService.getJokes().execute().getJoke()
-                );
-
-            } catch (IOException e) {
-                Log.e("Endpoint Executor", e.getMessage());
-                return "Unable to load data";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Intent intent = new Intent(getApplicationContext(),MainActivityDisplay.class);
-
-            intent.putExtra("JokeExtra",result);
-            startActivity(intent);
-
-        }
-    }
 
 }
 
